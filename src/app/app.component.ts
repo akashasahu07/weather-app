949df7744230cf9da2e780618a46e6271870e567
Akasha Sahu <akashasahu2001@gmail.com>
2025-02-05 23:31:20 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'weather';
+
+  heading:string = "weather app"
 }
