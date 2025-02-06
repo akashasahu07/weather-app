a3a2e53a2338d2649cb4b671ea277c869e69d6d3
Akasha Sahu <akashasahu2001@gmail.com>
2025-02-06 23:40:10 +0530

    Update main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,25 @@ export class AppComponent {
   heading: string = "weather app"
   searchCity: string = ""
   displayCity: string = ""
+  temp_c: string = ""
+  w_speed: string = ""
+  name: string = ""
+  region: string = ""
+  icon: string = ""
+
 
   showCity() {
-    this.displayCity = "Showing Data for: " +this.searchCity
+    this.displayCity = this.searchCity
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=cd4a9b6fe7e641859c474632250602&q=${this.displayCity}`)
+      .then((response) => response.json())
+
+      .then((data) => {
+        this.name = "Name: " + data.location.name
+        this.icon = data.current.condition.icon
+        this.region = "Region: " + data.location.region
+        this.temp_c = "Temperature: " + data.current.temp_c + " C"
+        this.w_speed = "Wind Speed: " + data.current.wind_kph + " kph"
+      })
   }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   constructor() { }
+
+  // showCity(city: string) {
+  //   this.displayCity = this.searchCity
+
+  //   fetch(`http://api.weatherapi.com/v1/current.json?key=cd4a9b6fe7e641859c474632250602&q=${this.displayCity}`)
+  //     .then((response) => response.json())
+  // }
 }
+
+
