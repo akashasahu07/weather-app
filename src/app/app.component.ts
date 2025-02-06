109b66ad71aee14b601c488437d8728b1d6001de
Akasha Sahu <akashasahu2001@gmail.com>
2025-02-06 13:55:29 +0530

    Update service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'weather';
 
-  heading:string = "weather app"
+  heading: string = "weather app"
+  searchCity: string = ""
+  displayCity: string = ""
+
+  showCity() {
+    this.displayCity = "Showing Data for: " +this.searchCity
+  }
 }
